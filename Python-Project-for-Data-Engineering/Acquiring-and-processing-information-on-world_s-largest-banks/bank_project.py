e65738a424a58e87aefcf73e842bd19740088057
kuuhaku <nguyenmanhdzung5920@gmail.com>
2023-11-06 09:46:09 +0700

    update Python-Project-for-Data-Engineering

--- Python-Project-for-Data-Engineering/Acquiring-and-processing-information-on-world_s-largest-banks/bank_project.py
+++ Python-Project-for-Data-Engineering/Acquiring-and-processing-information-on-world_s-largest-banks/bank_project.py
@@ -8,6 +8,7 @@ import numpy as np
 import sqlite3
 from datetime import datetime
 import warnings
+
 warnings.filterwarnings("ignore")
 
 url = "https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks"
@@ -33,17 +34,20 @@ def extract(url, table_attribs):
                 "MC_USD_Billion": float(col[2].get_text(strip=True)),
             }
             df1 = pd.DataFrame(data_dict, index=[0])
-            df = pd.concat([df, df1], ignore_index=True, join='inner')
+            df = pd.concat([df, df1], ignore_index=True, join="inner")
 
     return df
 
 
 def transform(df):
-    # GDP_list = df["GDP_USD_millions"].tolist()
-    # GDP_list = [float("".join(x.split(','))) for x in GDP_list]
-    # GDP_list = [np.round(x/1000,2) for x in GDP_list]
-    # df["GDP_USD_millions"] = GDP_list
-    # df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    exchange_rate = pd.read_csv("exchange_rate.csv")
+    exchange_rate = exchange_rate.set_index("Currency")["Rate"].to_dict()
+
+    for currency in exchange_rate:
+        df[f"MC_{currency}_Billion"] = df["MC_USD_Billion"].apply(
+            lambda x: np.round(x * exchange_rate[f"{currency}"], 2)
+        )
+
     return df
 
 
@@ -56,10 +60,9 @@ def load_to_db(df, sql_connection, table_name):
 
 
 def run_query(query_statement, sql_connection):
-    # print(query_statement)
-    # query_output = pd.read_sql(query_statement, sql_connection)
-    # print(query_output)
-    pass
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 
 
 def log_progress(message):
@@ -74,29 +77,34 @@ if __name__ == "__main__":
     # log_progress('Preliminaries complete. Initiating ETL process')
 
     df = extract(url, table_attribs)
-    print(df)
-    
-    # log_progress('Data extraction complete. Initiating Transformation process')
 
-    # df = transform(df)
+    log_progress("Data extraction complete. Initiating Transformation process")
 
-    # log_progress('Data transformation complete. Initiating loading process')
+    df = transform(df)
 
-    # load_to_csv(df, csv_path)
+    log_progress("Data transformation complete. Initiating loading process")
 
-    # log_progress('Data saved to CSV file')
+    load_to_csv(df, csv_path)
 
-    # sql_connection = sqlite3.connect('World_Economies.db')
+    log_progress("Data saved to CSV file")
 
-    # log_progress('SQL Connection initiated.')
+    sql_connection = sqlite3.connect(db_name)
 
-    # load_to_db(df, sql_connection, table_name)
+    log_progress("SQL Connection initiated.")
 
-    # log_progress('Data loaded to Database as table. Running the query')
+    load_to_db(df, sql_connection, table_name)
 
-    # query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
-    # run_query(query_statement, sql_connection)
+    log_progress("Data loaded to Database as table. Running the query")
 
-    # log_progress('Process Complete.')
+    query_statement = f"SELECT * FROM Largest_banks"
+    run_query(query_statement, sql_connection)
 
-    # sql_connection.close()
+    query_statement = f"SELECT AVG(MC_GBP_Billion) FROM Largest_banks"
+    run_query(query_statement, sql_connection)
+
+    query_statement = f"SELECT Name from Largest_banks LIMIT 5"
+    run_query(query_statement, sql_connection)
+
+    log_progress("Process Complete.")
+
+    sql_connection.close()
